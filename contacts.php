1b87b5131dbd585dee3d641fdc698896b2c9cf53
pishchulenok <64210553+sirejja@users.noreply.github.com>
2020-11-14 22:59:36 +0300

    Почти доделали контакты

--- contacts.php
+++ contacts.php
@@ -8,11 +8,8 @@
     
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="css/style.css">
-
   <link rel="stylesheet" type="text/css" href="css/style_contacts.css">
-
   <link rel="stylesheet" type="text/css" href="css/style-footer.css">
-  
   <link rel="stylesheet" type="text/css" href="css/style-banner.css">
   <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
   <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
