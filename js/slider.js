73e298f7879b39fea2b37d3c78a88ca760d7be12
pishchulenok <64210553+sirejja@users.noreply.github.com>
2020-11-14 14:57:23 +0300

    Initial commit

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 1500,
+    //dots: true,
+    //arrows: false,
+    cssEase: 'ease-in',
+    prevArrow: "<img src='img/arrow_l.png' class='prev' alt='1'>",
+    nextArrow: "<img src='img/arrow_r.png' class='next' alt='2'>",
+});
